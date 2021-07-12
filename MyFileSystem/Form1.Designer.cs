7cf58ad2791c1bfa7bb863713c7878b047e80c82
leonjye@ntub.edu.tw <leonjye@gmail.com>
2021-07-12 15:06:12 +0800

    DirectoryInfo

---
+++ MyFileSystem/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace MyFileSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("新細明體", 14F);
+            this.lblMsg.Location = new System.Drawing.Point(12, 9);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(178, 24);
+            this.lblMsg.TabIndex = 0;
+            this.lblMsg.Text = "程式執行的訊息";
+            // 
+            // btnAction
+            // 
+            this.btnAction.Font = new System.Drawing.Font("新細明體", 14F);
+            this.btnAction.Location = new System.Drawing.Point(316, 9);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(159, 33);
+            this.btnAction.TabIndex = 1;
+            this.btnAction.Text = "測試";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 338);
+            this.Controls.Add(this.btnAction);
+            this.Controls.Add(this.lblMsg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button btnAction;
+    }
+}
+
---
+++ MyFileSystem/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace MyFileSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\Users\leonjye\Downloads\diricons2");
+            dirInfo.MoveTo(@"C:\Users\leonjye\Downloads\myicons0304");
+            lblMsg.Text = @"移動到C:\Users\leonjye\Downloads\myicons0304";
+            //dirInfo.Delete();
+        }
+    }
+}
