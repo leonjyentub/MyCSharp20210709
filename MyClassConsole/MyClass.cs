e39252c0f83d7f57de526d14616385a53a7831c7
leonjye@ntub.edu.tw <leonjye@gmail.com>
2021-07-13 14:50:21 +0800

    merge

--- MyClassConsole/MyClass.cs
+++ MyClassConsole/MyClass.cs
@@ -6,41 +6,5 @@ using System.Threading.Tasks;
 
 namespace MyClassConsole
 {
-    class MyClass
-    {
-        public MyClass()
-        {
-            this.Name = "";
-            this._score = 0;
-        }
-
-        public MyClass(String name, int myscore)
-        {
-            this.Name = name;
-            this._score = myscore;
-        }
-        public String Name { get; set; }
-        private int _score;  //一般普通欄位
-        public int Score
-        {
-            get { return this._score; }
-            set
-            {
-                if(_score >= 0 && _score <= 100)
-                {
-                    this._score = value;
-                }
-                else
-                {
-                    Console.WriteLine("設定錯誤");
-                    this._score = 0;
-                }
-            }
-        }
-       
-        public void HelloWorldTest()
-        {
-            Console.WriteLine($"我是{this.Name}分數是{this._score}");
-        }
-    }
+    
 }
