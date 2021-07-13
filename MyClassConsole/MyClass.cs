24f5842b5ab31b1cd7c349d2ac9ac9f16e40d050
leonjye@ntub.edu.tw <leonjye@gmail.com>
2021-07-13 14:03:45 +0800

    first Class

---
+++ MyClassConsole/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassConsole
+{
+    class MyClass
+    {
+        private int a;
+
+        public int A { get; set; }
+        public void HelloWorldTest()
+        {
+            Console.WriteLine($"A = {this.A}");
+        }
+    }
+}
---
+++ MyClassConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassConsole
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass my = new MyClass();
+            my.A = 10;
+            my.HelloWorldTest();
+            Console.ReadKey();
+        }
+    }
+}
