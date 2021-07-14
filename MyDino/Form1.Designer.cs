aa7a817471f7bb6ea64f12bd5ab948f7d2a1c8ee
leonjye@ntub.edu.tw <leonjye@gmail.com>
2021-07-14 16:53:50 +0800

    my dino

--- MyDino/Form1.Designer.cs
+++ MyDino/Form1.Designer.cs
@@ -30,30 +30,32 @@ namespace MyDino
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picDino = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.timerJump = new System.Windows.Forms.Timer(this.components);
             this.timerRoad = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.picTree = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picDino)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // picDino
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.picDino.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.pictureBox1.Image = global::MyDino.Properties.Resources.running;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 76);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(86, 85);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.picDino.Image = global::MyDino.Properties.Resources.running;
+            this.picDino.Location = new System.Drawing.Point(12, 169);
+            this.picDino.Name = "picDino";
+            this.picDino.Size = new System.Drawing.Size(85, 88);
+            this.picDino.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDino.TabIndex = 0;
+            this.picDino.TabStop = false;
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 167);
+            this.panel1.Location = new System.Drawing.Point(0, 263);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 47);
             this.panel1.TabIndex = 1;
@@ -62,27 +64,45 @@ namespace MyDino
             // 
             this.timerJump.Tick += new System.EventHandler(this.timerJump_Tick);
             // 
+            // timerRoad
+            // 
+            this.timerRoad.Tick += new System.EventHandler(this.timerRoad_Tick);
+            // 
+            // picTree
+            // 
+            this.picTree.Image = global::MyDino.Properties.Resources.obstacle_1;
+            this.picTree.Location = new System.Drawing.Point(765, 211);
+            this.picTree.Name = "picTree";
+            this.picTree.Size = new System.Drawing.Size(23, 46);
+            this.picTree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picTree.TabIndex = 2;
+            this.picTree.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 214);
+            this.ClientSize = new System.Drawing.Size(800, 310);
+            this.Controls.Add(this.picTree);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picDino);
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDino)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picTree)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picDino;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Timer timerJump;
         private System.Windows.Forms.Timer timerRoad;
+        private System.Windows.Forms.PictureBox picTree;
     }
 }
 
--- MyDino/Form1.cs
+++ MyDino/Form1.cs
@@ -12,19 +12,48 @@ namespace MyDino
 {
     public partial class Form1 : Form
     {
+        private bool isUp = false;
+        private int startTop = 0;
+        private int treeLeft = 0;
         public Form1()
         {
             InitializeComponent();
+            startTop = picDino.Top;
+            treeLeft = picTree.Left;
+            timerRoad.Enabled = true;
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-
+            if(e.KeyCode == Keys.Space)
+            {
+                isUp = true;
+                timerJump.Enabled = true;
+            }
         }
 
         private void timerJump_Tick(object sender, EventArgs e)
         {
+            if (picDino.Top < 30) 
+                isUp = false;
+            picDino.Top += isUp ? -10 : 10;
+            if (picDino.Top >= startTop)
+                timerJump.Enabled = false;
+        }
 
+        private void timerRoad_Tick(object sender, EventArgs e)
+        {
+            picTree.Left -= 20;
+            if(picDino.Bounds.IntersectsWith(picTree.Bounds))
+            {
+                this.Text = "失敗~";
+                timerJump.Stop();
+                timerRoad.Stop();
+            }
+            if(picTree.Left <= 0)
+            {
+                picTree.Left = treeLeft;
+            }
         }
     }
 }
